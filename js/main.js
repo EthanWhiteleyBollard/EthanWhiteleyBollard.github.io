ea33678da60e52ff2acc633548a868988d2e2976
Kerminty <110630829+Kerminty@users.noreply.github.com>
2025-04-20 14:54:24 +0100

    Scroll to navbar and mobile update

--- js/main.js
+++ js/main.js
@@ -4,4 +4,33 @@ document.addEventListener('DOMContentLoaded', () =>{
     const mobileMenu = document.querySelector('.mobile-menu');
      
     hamburgerButton.addEventListener('click', () => mobileMenu.classList.toggle('active'));
+
+    //About Me
+    const aboutMe = document.querySelector('.hero');
+    const aboutMeButton = document.querySelector('.about-me-button-mm');
+
+    aboutMeButton.addEventListener('click', () => {aboutMe.scrollIntoView({behavior: "smooth", block: "start"})});
+    aboutMeButton.addEventListener('click', () => mobileMenu.classList.toggle('active'));
+
+    //Projects
+    const projects = document.querySelector('.project');
+    const projectsButton = document.querySelector('.projects-button-mm');
+
+    projectsButton.addEventListener('click', () => {projects.scrollIntoView({behavior: "smooth", block: "start"})}, () => mobileMenu.classList.toggle('active'));
+    projectsButton.addEventListener('click', () => mobileMenu.classList.toggle('active'));
+})
+
+// Scroll
+document.addEventListener('DOMContentLoaded', () =>{
+    //About Me
+    const aboutMe = document.querySelector('.hero');
+    const aboutMeButton = document.querySelector('.about-me-button');
+
+    aboutMeButton.addEventListener('click', () => {aboutMe.scrollIntoView({behavior: "smooth", block: "start"})});
+
+    //Projects
+    const projects = document.querySelector('.project');
+    const projectsButton = document.querySelector('.projects-button');
+
+    projectsButton.addEventListener('click', () => {projects.scrollIntoView({behavior: "smooth", block: "start"})});
 })
